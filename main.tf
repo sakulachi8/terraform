c3bb6372520f6b4b7cf3bd93b83d11c2d665cdb7
sakulachi8 <triplektechnologies@sakulachi8.local>
2020-12-25 21:42:53 +0500

    m

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 provider "azurerm" {
   features {}
+
+  subscription_id = "5c4b1a1c-000e-4f7a-91a5-6776165fd1dd"
+  client_id       = "8cf0fa37-6205-43f7-b06b-3cfb59026540"
+  client_secret   = var.client_secret
+  tenant_id       = "1749fb38-fafd-46ef-bfa9-c593ae55e03f"
 }
 
 resource "azurerm_resource_group" "basic" {
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "resource_group" {
+  description = "The name of your Azure Resource Group."
+  default     = "vm-creation-random"
+}
+
+variable "rg_location" {
+  description = "The region of RG"
+  default     = "centralus"
+}
